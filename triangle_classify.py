b6a3af5ae653f1b187b2aa47516042fbaa8b8d74
Eduards <eduardsosh@gmail.com>
2024-05-13 15:12:16 +0300

    Added triangle type checks

--- triangle_classify.py
+++ triangle_classify.py
@@ -1,13 +1,22 @@
+"""
+Python script to get the type of triangle from side lenghts a,b and c
+"""
 import math
 
+TOLERANCE = 1e-8
+
 def lt_or_equals(a,b)->bool:
-    TOLERANCE = 1e-8
     return a <= b or math.isclose(a, b, rel_tol=TOLERANCE, abs_tol=TOLERANCE)
+
+def equals(a,b)->bool:
+    return math.isclose(a,b,rel_tol=TOLERANCE,abs_tol=TOLERANCE)
 
 
 def classify_triangle(a, b, c) -> str:
-    #Initial input sanitation
-    errors = list()
+    # Let , also work as a decimal seperator
+    a = a.replace(',','.')
+    b = b.replace(',','.')
+    c = c.replace(',','.')
 
     inputs = {'a': a, 'b': b, 'c': c}
     for var_name, value in inputs.items():
@@ -15,8 +24,15 @@ def classify_triangle(a, b, c) -> str:
             float(value)
         except ValueError:
             return f"Error in input '{var_name}': not a valid floating-point number!"
-        
-    # Check if a triangle exists
+    
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    
+    MAX_LENGHT = 1_000_000_000.0
+    if(a > MAX_LENGHT or b > MAX_LENGHT or c > MAX_LENGHT):
+        return "Error: Side/s too long!"
+    
     if not(a > 0 and b > 0 and c > 0):
         return "Error: Invalid side lenght/s!"
     
@@ -24,12 +40,37 @@ def classify_triangle(a, b, c) -> str:
     if (lt_or_equals(a+b,c) or lt_or_equals(a+c,b) or lt_or_equals(c+b,a)):
         return "Error: Triangle doesn't exist!"
     
-    # Make a max limit on the side lenght
-    MAX_LENGHT = 1_000_000_000
-    if(a > MAX_LENGHT or b > MAX_LENGHT or c > MAX_LENGHT):
-        return "Error: Side/s too long!"
+    
+    # is equilateral?
+    if(equals(a,b) and equals(b,c)):
+        return "Equilateral Triangle"
+    
+    # is Isosceles?
+    if(equals(a,b) or equals(b,c) or equals(a,c)):
+        return "Isosceles Triangle"
+    else:
+        return "Scalene Triangle"
     
 
 
-print(classify_triangle("10", "20", "30"))
-print(classify_triangle("10", "20x", "30"))
+print(classify_triangle("-1", "2", "2"))
+print(classify_triangle("1", "20", "15,5"))
+print(classify_triangle("1000000000000000", "20", "15,5"))
+print(classify_triangle("15.5", "20", "15,5"))
+print(classify_triangle("0.001", "0.001", "0.001"))
+print(classify_triangle("3", "4", "5"))
+print(classify_triangle("-1", "2", "2"))
+
+def test_classify(*additional):
+    inputs = [("-1", "2", "2"),
+              ("1", "20", "15,5"),
+              ("1000000000000000", "20", "15,5"),
+              ("15.5", "20", "15,5"),
+              ("0.001", "0.001", "0.001"),
+              ("3", "4", "5"),
+              ("-1", "2", "2")]
+    
+    inputs.append(additional)
+
+
+
